9851c8f2300a03d65cd1f8499fd1db1456b80b84
CNagal <christiannagal@gmail.com>
2016-08-03 15:23:23 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function formatWeight() {
+	
+	var yourWeight = document.getElementById("weight").value;
+	var planetGravity = document.getElementById("planets").value;
+	var newWeight = parseInt(yourWeight);
+	var formatWeight = newWeight * planetGravity;
+
+	document.getElementById("p1").innerHTML = ("Your new weight is " + formatWeight);
+
+}
